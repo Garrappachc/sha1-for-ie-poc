dae2ebae96ad836e2ee1154a7ff1dc2b467d9315
Michał <michal@garapich.pl>
2018-10-10 14:33:41 +0200

    SHA-1 initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TextEncoder } from 'text-encoding';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sha1poc';
+export class AppComponent implements OnInit {
+
+  input = 'dupa13';
+
+  output = '';
+
+  ngOnInit() {
+    const encoded = new TextEncoder().encode(this.input);
+    window.crypto.subtle.digest('SHA-1', encoded)
+      .then(hash => {
+        console.log('HASHED');
+        this.output = String.fromCharCode.apply(null, new Uint8Array(hash));
+      });
+  }
+
 }
